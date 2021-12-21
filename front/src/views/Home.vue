c31c679dbc8aeddfc27b9afc375ca474fe14aeca
Lucas Laurillault <lucas@macbook-air-de-lucas.home>
2021-12-21 16:53:06 +0100

    create components nav

--- front/src/views/Home.vue
+++ front/src/views/Home.vue
@@ -11,14 +11,7 @@
             <h1 class="description__title">lucas laurillault</h1>
             <p class="description__paragraph">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cupiditate maxime perspiciatis facilis nesciunt id deserunt ea sapiente voluptates quisquam.</p>
         </div>
-            <nav>
-                <ul>
-                    <li><a class="about__link" href="#about">A propos</a></li>
-                    <li><a class="work__link" href="#work">Projets</a></li>
-                    <li><a class="cv__link" href="#cv">CV</a></li>
-                    <li><a class="contact__link" href="#contact">Contact</a></li>
-                </ul>
-            </nav>
+            <Nav/>
     </main>
 
     <footer>
@@ -29,8 +22,13 @@
 </template>
 
 <script>
+import Nav from '../components/Nav'
+
 export default {
   name: 'Home',
+  components: {
+    Nav
+  },
   beforeCreate () {
     document.querySelector('body').setAttribute('style', 'margin: 0')
   },
@@ -94,39 +92,4 @@ footer{
   letter-spacing: 0.2rem;
   margin-bottom: 2.5rem;
 }
-
-nav{
-  margin-top: 3rem;
-}
-
-ul{
-  display: flex;
-  justify-content: center;
-}
-
-li{
-  list-style: none;
-}
-
-a{
-  display: block;
-  color: #ffff;
-  text-decoration: none;
-  padding: 1rem 1.25rem 1rem 1.45rem;
-  border: solid #ffff 1px;
-  font-size: 13px;
-  letter-spacing: 0.2rem;
-  min-width: 5rem;
-  text-align: center;
-}
-
-a:hover{
-  background-color: #787880;
-  transition: 1s;
-}
-
-.about__link, .work__link, .cv__link{
-  border-right: none;
-}
-
 </style>
