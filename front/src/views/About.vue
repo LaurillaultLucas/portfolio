966e9779d279e005b4fb1a89df17c034a31a0386
Lucas Laurillault <lucas@macbook-air-de-lucas.home>
2022-01-08 20:45:00 +0100

    about page done

--- front/src/views/About.vue
+++ front/src/views/About.vue
@@ -2,11 +2,11 @@
   <div class="about">
       <Header />
       <main>
-          <article class="front-end">
+          <section class="front-end">
               <i class="fab fa-js"></i>
               <h1>Front-end</h1>
               <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsam amet reiciendis iste? Blanditiis, corrupti. Inventore odit velit soluta iure voluptas! Eum doloremque quis dolorem id debitis eligendi rem minus voluptatem.</p>
-              <h2>Langages</h2>
+              <h2>Compétences</h2>
               <ul>
                   <li>HTML</li>
                   <li>CSS</li>
@@ -14,17 +14,32 @@
                   <li>Javascript</li>
                   <li>VueJS</li>
               </ul>
-          </article>
-          <article class="back-end">
+          </section>
+          <section class="back-end">
               <i class="fab fa-php"></i>
               <h1>Back-end</h1>
               <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique provident neque, repudiandae, numquam, beatae sequi aliquam sit ipsam nobis vitae voluptate et qui. Molestiae ab blanditiis provident impedit reprehenderit modi.</p>
-          </article>
-          <article class="various">
+              <h2>Compétences</h2>
+              <ul>
+                  <li>PHP</li>
+                  <li>Wordpress</li>
+                  <li>MySQL</li>        
+                  <li>MCD</li>
+              </ul>
+          </section>
+          <section class="tools">
               <i class="fas fa-gamepad"></i>
-              <h1>Divers</h1>
-              <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsam amet reiciendis iste? Blanditiis, corrupti. Inventore odit velit soluta iure voluptas! Eum doloremque quis dolorem id debitis eligendi rem minus voluptatem.</p>
-          </article>
+              <h1>Outils</h1>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique provident neque, repudiandae, numquam, beatae sequi aliquam sit ipsam nobis vitae voluptate et qui. Molestiae ab blanditiis provident impedit reprehenderit modi.</p>
+              <h2>Je travail avec</h2>
+              <ul>
+                  <li>VSCode</li>
+                  <li>Github</li>
+                  <li>Mac OS/Linux/Windows</li>        
+                  <li>Terminal</li>
+                  <li>Whimsical</li>
+              </ul>
+          </section>
       </main>
       <Footer />
   </div>
@@ -52,7 +67,7 @@ export default {
         margin: auto;
         margin-bottom: 2.5rem;
 
-        article{
+        section{
             text-align: center;
             color: #ffff;
             padding: 0rem 2rem 0rem 2rem;
@@ -61,8 +76,15 @@ export default {
                 font-size: 3rem;
             }
 
-            h1, h2{
+            h1{
                 letter-spacing: 0.2rem;
+                margin-top: 3rem;
+            }
+
+            h2{
+                letter-spacing: 0.2rem;
+                margin-top: 3rem;
+                font-size: 1.3rem;
             }
             
             ul{
