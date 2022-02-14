19110e0709d7c2ec4372e67e27f95b1057c8c22f
Lucas Laurillault <lucas@macbook-air-de-lucas.home>
2022-02-14 19:04:16 +0100

    amelioration

---
+++ front/src/services/postService.js
@@ -0,0 +1,14 @@
+import axios from 'axios';
+
+const postService = {
+    baseURI: 'http://localhost:8080/portfolio/Back/web/wp-json/wp/v2',
+
+        async loadPosts(){
+            console.log('On est dans loadPost !!!');
+            const response = await axios.get(postService.baseURI + '/posts');
+            console.log(response.data);
+            return response.data;
+        },
+};
+
+export default postService;
